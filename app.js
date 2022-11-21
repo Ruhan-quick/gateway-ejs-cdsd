50e6c9b24b85cadcdd9ebed9f09a4adadcfd0527
ruhan-quick <ruhanquick@gmail.com>
2022-11-21 07:04:22 +0600

    use of ejs started

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser);
+
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'title',
+    title: 'gateway1 ejs',
   });
+});
+
+app.post('/panel', (req, res, next) => {
+  console.log(req.body.Email);
+  next();
 });
 
 app.listen(3000);
